01bf7b46c421f4301c96714dc2e6d012fcaa45a1
VickoT <vicko.tornblom@gmail.com>
2024-10-17 12:08:40 +0200

    add loop script for re_eval

--- re_eval.py
+++ re_eval.py
@@ -12,10 +12,13 @@ from datetime import datetime
 
 try:
     with open('report.txt', 'r') as file:
-        line = file.readlines()
-        # fetch the line containing sample volume 
-        volume_line = line[2]
-        sample_volume_imaged = volume_line.split(':')[1].strip()
+        lines = file.readlines()
+        # Fetch the last line containing the sample data
+        last_line = lines[-1]
+        # Split the last line by tab to get the values
+        last_line_parts = last_line.split()
+        sample = last_line_parts[0]
+        volume_imaged = float(last_line_parts[1])
         
 except FileNotFoundError:
     print(f"The file {file_path} does not exist.")
@@ -24,7 +27,7 @@ current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
 count_protist = len(os.listdir('Predicted_images/Protist'))
 count_missed = len(os.listdir('Predicted_images/Missed'))
 count_junk = len(os.listdir('Predicted_images/Junk'))
-sample_volume_imaged = float(sample_volume_imaged)
+volume_imaged = float(volume_imaged)
 
 text_to_append = [
     "\n---------------------------------------------------------------",
@@ -33,7 +36,8 @@ text_to_append = [
     f"Junk: {count_junk}",
     f"Missed: {count_missed}",
     f"Protist: {count_protist}",
-    f"\nEstimated conc. (protists/ml): {count_protist/sample_volume_imaged:.2f}"
+    f"\nEstimated conc. (protists/ml): {count_protist/volume_imaged:.2f}",
+    f"\n{sample}\t{volume_imaged}\t{count_junk}\t{count_missed}\t{count_protist}\t{count_protist / volume_imaged:.2f}"
 ]
 
 with open('report.txt', 'a') as file:
